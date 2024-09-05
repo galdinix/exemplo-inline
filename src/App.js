72d0ee0627ea6117952a03448a4fa5c6d1c70290
galdinix <lsgaldino27@gmail.com>
2024-09-05 11:20:07 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const conteinerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  backgroundColor: "#f0f0f0",
+  textAling: "center",
+}
+const buttonStyle = {
+  backgroundColor: 'blue',
+  widght: '20px',
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '10px',
+  cursor: 'pointer',
+  marginTop: '20px',
+  fontSize: '16px',
+  fontWeight: 'bold'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={conteinerStyle}>
+    <h1 style={{color: '#333', fontSize: '24px'}}>
+      Estilos de inline em React
+    </h1>
+    <p style={{color: '#666', maxWidth: '600px'}}>
+      Exemplo de estilo inline
+    </p>
+    <button style={buttonStyle} onClick={() => alert('voce clicou no botão')}>
+      Clique aqui 
+
+    </button>
+
+   </div>
   );
 }
 
